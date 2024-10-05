c9e5cf68528837bcf4eed15cb63116ec6e96f39f
ssinha8752 <shubhamsinha8752@yahoo.com>
2024-10-05 20:34:50 +0800

    [Shubham][3-Oct] Day 4 - Python

---
+++ Day-4.py
@@ -0,0 +1,14 @@
+#random
+import random
+
+p=random.randint(0,1)
+if p==0:
+    print("Heads")
+else:
+    print("Tails")
+
+
+#List
+fruits=['A','B','C','D','E']
+p=random.randint(0,4)
+print(fruits[p])
