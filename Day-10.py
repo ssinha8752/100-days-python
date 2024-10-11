4a75769194d60ae0ef5f685512f538aefa84fd9b
ssinha8752 <shubhamsinha8752@yahoo.com>
2024-10-11 22:37:27 +0800

    [Shubham][10-Oct] Day 10 - Python

---
+++ Day-10.py
@@ -0,0 +1,23 @@
+def format_name(fname, lname):
+    print(f"{fname.title()} {lname.title()}")
+
+format_name("SHauhyeas",'SfvweaDE')
+
+
+def f1(text):
+    return text + text
+
+def f2(text):
+    return text
+
+output=f2(f1("Hi"))
+print(output)
+
+
+#docstring
+"""Adding text between 3 double quotes """
+
+#or
+# use crtl + /
+# for multiple lines
+# to get hashtag
