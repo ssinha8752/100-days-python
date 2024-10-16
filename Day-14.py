820751f941e96830a63e28d33cb29db1d9adf209
ssinha8752 <shubhamsinha8752@yahoo.com>
2024-10-16 23:56:19 +0800

    [Shubham][16-Oct] Day 14 - Python

---
+++ Day-14.py
@@ -0,0 +1,9 @@
+#TODO
+1. create a dictionary with key and values, game=true
+2. random function to start with one item from dictionary
+3. counter=0
+4. randomly bring the next item from the dictionary
+5. if selected correctly then it becomes current
+6. and counter + 1
+7.once wrong, game=false
+8. Show score
