7886d83745f6342592a23a874e010d3f4547c609
ssinha8752 <shubhamsinha8752@yahoo.com>
2024-10-22 21:32:55 +0800

    [Shubham][18-Oct] Day 17 - Learning regex

--- Assignment2-Q1.py
+++ Assignment2-Q1.py
@@ -5,16 +5,20 @@ class Q1:
 
         ### REPLACE THE BELOW LINE WITH YOUR CODE ###
         ### DO NOT EDIT ANYTHING ELSE IN THIS FUNCTION ###
-        self.pattern = r'(?<!00\.)\b(?!00)\d{1,3}\.\d{2}\b'
+        #self.pattern = r'(?<!00\.)\b(?!00)(?<!\d)\d{2}\.\d{2}(?!\d)'
+        #self.pattern =  r'(?<!00\.)\b(?!00)(?<!\d)\d{2}\.\d{2}(?!\d)'
+        #self.pattern = r'(?<!00\.)\b(?!00)(?<!\d)(\d{2}\.\d{2})(?!\d)\b'
+        #self.pattern = r'(?<!00\.)\b(?!00)(?<!\d)(\d{2}\.\d{2})(?!\d)(?=\D|$)'
+        self.pattern  = r'(?<!\d)([1-9]\d?\.\d{2})(?=\D|$)'
         next = "00.00+00.00"
         while next:
             p = re.compile(self.pattern)
-            print(p)
             m = p.findall(d[next])
             print(m)
             if not m:
                 break
             next = "+".join(m)
+            print(str(next))
 
         return next
 
@@ -22,8 +26,9 @@ class Q1:
 # Run this code to get the feedback for question 1
 with open('q1.txt', 'w') as f:
     score = 0
+
     q1 = Q1()
-    ### Open test case # 1 ###
+    ### open test case # 1 ###
     d = {
         "00.00+00.00": "The next treasure is at 12.34 and 23.45",
         "12.34+23.45": "The fake treasure is in 222.11 and 11.11 and 14.56",
@@ -37,7 +42,7 @@ with open('q1.txt', 'w') as f:
     else:
         f.write('[0/1] first sample case is not matched\n')
 
-    ### Open test case # 2 ###
+    ### open test case # 2 ###
     d = {
         "00.00+00.00": "The next treasure is at 16.88 and 26.84",
         "16.88+26.84": "The fake treasure is in 00.13 and 55.66 and 14.56",
@@ -52,12 +57,9 @@ with open('q1.txt', 'w') as f:
     else:
         f.write('[0/1] second sample case is not matched\n')
 
-    ### Test that the length of pattern is less than or equal to 50 characters ###
+    ### test that the length of pattern is less than or equal to 50 characters ###
     if len(q1.pattern) <= 50:
         score += 1
         f.write('[1/1] pattern is less than or equal to 50 characters\n')
     else:
         f.write('[0/1] pattern is longer than 50 characters\n')
-
-# Output the final score
-print(f"Final Score: {score}/3")
