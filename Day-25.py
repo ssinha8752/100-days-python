8ffb695ff67ee4b7d07cb3ce5c64988d8e531ea2
ssinha8752 <shubhamsinha8752@yahoo.com>
2024-10-27 22:05:45 +0800

    [27-Oct][Shubham] Working with Pandas

--- Day-25.py
+++ Day-25.py
@@ -10,12 +10,12 @@ with open ("./.venv/weather.csv") as datafile:
     print(temp)
 
 df=pd.read_csv("weather_data.csv")
-print(df['temp'])
-print(df['temp'].to_dict())
-print(df['temp'].mean())
-print(df['temp'].max())
-print(df[df.day=='Monday'])
-print(df[df.temp==max(df['temp'])])
+print(df['temp']) #to get temp column
+print(df['temp'].to_dict()) #conver into dictionary
+print(df['temp'].mean()) #find mean of all temp
+print(df['temp'].max()) #find max of all temp
+print(df[df.day=='Monday']) # row where day is monday
+print(df[df.temp==max(df['temp'])]) #row where day has max temp
 mon=df[df.day=='Monday']
-print(mon.condition)
-print((mon.temp[0]*9/5)+32)
+print(mon.condition) #getting condition for monday
+print((mon.temp[0]*9/5)+32) #converting temp for c to F
