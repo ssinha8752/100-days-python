521598ebe65e6f6d42e807272b199647dd67be2a
ssinha8752 <shubhamsinha8752@yahoo.com>
2024-12-18 22:32:45 +0800

    [Shubham][18-12] Single Linked List Remove

--- Single LL.py
+++ Single LL.py
@@ -103,6 +103,21 @@ class ll:
         self.length+=1
         return True
 
+    def remove(self,index):
+        if index<self.length<0 or index>=self.length:
+            return None
+        if index==0:
+            self.pop_first()
+        if index==self.length-1:
+            self.pop()
+        prev=self.get(index-1)
+        temp=prev.next
+        prev.next=temp.next
+        temp.next=None
+        self.length-=1
+        return temp
+
+
 my_ll=ll(1)
 my_ll.append(2)
 my_ll.append(4)
