89ffd655086d47746d622ab5c8dfebe72e9c00d6
ssinha8752 <shubhamsinha8752@yahoo.com>
2024-12-26 20:50:26 +0800

    [Shubham][26-12] Single Linked List Pop first - Quiz

--- Single LL/Single-LL Pop First.py
+++ Single LL/Single-LL Pop First.py
@@ -55,12 +55,16 @@ class LinkedList:
         self.length += 1
         return True
 
-    ## WRITE POP_FIRST METHOD HERE ##
-    #                               #
-    #                               #
-    #                               #
-    #                               #
-    #################################
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
 
 
 my_linked_list = LinkedList(2)
@@ -72,12 +76,3 @@ print(my_linked_list.pop_first().value)
 print(my_linked_list.pop_first().value)
 # (0) Items - Returns None
 print(my_linked_list.pop_first())
-
-"""
-    EXPECTED OUTPUT:
-    ----------------
-    2
-    1
-    None
-
-"""
