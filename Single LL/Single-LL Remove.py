0afa3722d49a30ed8a99554ae2e040784af083e1
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-01-02 19:02:09 +0800

    [Shubham][2-1] Single Linked List Remove - Quiz

--- Single LL/Single-LL Remove.py
+++ Single LL/Single-LL Remove.py
@@ -96,17 +96,20 @@ class LinkedList:
         return True
 
     def remove(self, index):
-        if index<0 or index>self.length:
+        if index<0 or index>=self.length:
             return None
-        else:
-            temp=self.get(index-1)
-            curr=temp.next
-            temp.next=curr.next
-            curr.next=None
-            self.length -= 1
-            if self.length == 0:
-                self.tail = None
-            return curr
+        if index==0:
+            return self.pop_first()
+        if index==self.length-1:
+            return self.pop()
+        temp=self.get(index-1)
+        curr=temp.next
+        temp.next=curr.next
+        curr.next=None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return curr
 
 
 my_linked_list = LinkedList(1)
