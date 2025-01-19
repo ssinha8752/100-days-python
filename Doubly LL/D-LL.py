8c9320de2b2e7cb5d33c374f7b9a1284dd125adb
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-01-19 22:32:26 +0800

    [Shubham][19-1] Doubly Linked List

--- Doubly LL/D-LL.py
+++ Doubly LL/D-LL.py
@@ -19,5 +19,17 @@ class DoublyLinkedList:
             print(temp.value)
             temp=temp.next
 
+    def append(self, value):
+        new_node=Node(value)
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+        else:
+            self.tail.next=new_node
+            new_node.prev=self.tail
+            self.tail=new_node
+        self.length+=1
+        return True
+
 my_dll=DoublyLinkedList(7)
 my_dll.print_list()
