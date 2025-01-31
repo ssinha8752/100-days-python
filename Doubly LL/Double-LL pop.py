3db026853e33050b0a6fbeaaf8972c5c9b389673
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-01-31 23:16:38 +0800

    [Shubham][31-1] Doubly Linked List - Pop

--- Doubly LL/Double-LL pop.py
+++ Doubly LL/Double-LL pop.py
@@ -18,37 +18,41 @@ class DoublyLinkedList:
             print(temp.value)
             temp = temp.next
 
-    def append(self,val):
-        new_node=Node(val)
+    def append(self, value):
+        new_node = Node(value)
         if self.head is None:
             self.head = new_node
             self.tail = new_node
         else:
-            self.tail.next=new_node
-            new_node.prev=self.tail
-            self.tail=new_node
-        self.length+=1
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    ### WRITE POP METHOD HERE ###
+    #                           #
+    #                           #
+    #                           #
+    #                           #
+    #############################
 
 
 my_doubly_linked_list = DoublyLinkedList(1)
 my_doubly_linked_list.append(2)
 
-print('Head:', my_doubly_linked_list.head.value)
-print('Tail:', my_doubly_linked_list.tail.value)
-print('Length:', my_doubly_linked_list.length, '\n')
-
-print('Doubly Linked List:')
-my_doubly_linked_list.print_list()
+# (2) Items - Returns 2 Node
+print(my_doubly_linked_list.pop().value)
+# (1) Item -  Returns 1 Node
+print(my_doubly_linked_list.pop().value)
+# (0) Items - Returns None
+print(my_doubly_linked_list.pop())
 
 """
     EXPECTED OUTPUT:
     ----------------
-    Head: 1
-    Tail: 2
-    Length: 2 
-
-    Doubly Linked List:
-    1
     2
+    1
+    None
 
 """
