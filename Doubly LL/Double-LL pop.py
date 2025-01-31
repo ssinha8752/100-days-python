a7de124356f6d62618864c54b56649902388da9d
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-01-31 23:26:11 +0800

    [Shubham][31-1] Doubly Linked List - Pop

--- Doubly LL/Double-LL pop.py
+++ Doubly LL/Double-LL pop.py
@@ -30,12 +30,19 @@ class DoublyLinkedList:
         self.length += 1
         return True
 
-    ### WRITE POP METHOD HERE ###
-    #                           #
-    #                           #
-    #                           #
-    #                           #
-    #############################
+    def pop(self):
+        if self.length==0:
+            return None
+        temp=self.tail
+        if self.length==1:
+            self.head=None
+            self.tail=None
+        else:
+            self.tail=temp.prev
+            self.tail.next=None
+            temp.prev=None
+        self.length-=1
+        return temp
 
 
 my_doubly_linked_list = DoublyLinkedList(1)
