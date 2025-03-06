17bf73ed67914a306d005f7fa4b17e5b485ed3e0
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-03-06 19:45:41 +0800

    [Shubham][6-3] Stack - Pop

--- Stacks/Quiz- Stack pop.py
+++ Stacks/Quiz- Stack pop.py
@@ -26,12 +26,15 @@ class Stack:
         self.height += 1
         return True
 
-    ### WRITE POP METHOD HERE ###
-    #                           #
-    #                           #
-    #                           #
-    #                           #
-    #############################
+    def pop(self):
+        if self.height==0:
+            return None
+        else:
+            temp=self.top
+            self.top=self.top.next
+            temp.next=None
+        self.height-=1
+        return temp
 
 
 my_stack = Stack(4)
