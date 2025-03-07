d09077780894e1ebfa24cac2c925d3bdd09907dc
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-03-07 23:42:45 +0800

    [Shubham][7-3] Queue - Constructor

---
+++ Queue/Quiz - Queue Constructor.py
@@ -0,0 +1,31 @@
+# class Node:
+## WRITE NODE CONSTRUCTOR HERE ##
+#                               #
+#                               #
+#                               #
+#                               #
+#################################
+
+# class Queue:
+## WRITE QUEUE CONSTRUCTOR HERE ##
+#                                #
+#                                #
+#                                #
+#                                #
+##################################
+
+
+my_queue = Queue(4)
+
+print('First:', my_queue.first.value)
+print('Last:', my_queue.last.value)
+print('Length:', my_queue.length)
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    First: 4
+    Last: 4
+    Length: 1
+
+"""
