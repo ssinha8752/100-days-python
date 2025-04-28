2196c948c9d8dfc79ff4a66eb79fa152b4dadc2a
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-04-28 23:30:56 +0800

    [Shubham][28-4] Hash Table - Common

--- Hash Table/Quiz - HT Common.py
+++ Hash Table/Quiz - HT Common.py
@@ -1,12 +1,11 @@
-# WRITE ITEM_IN_COMMON FUNCTION HERE #
-#                                    #
-#                                    #
-#                                    #
-#                                    #
-######################################
-
-
-
+def item_in_common(l1,l2):
+    dict={}
+    for i in l1:
+        dict[i]=True
+    for j in l2:
+        if j in dict:
+            return True
+    return False
 
 list1 = [1,3,5]
 list2 = [2,4,5]
