5068c081d196e81f647af25660c8635f2c6db19a
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-04-28 23:19:43 +0800

    [Shubham][27-4] Hash Table - Quiz

---
+++ Hash Table/Quiz - HT Common.py
@@ -0,0 +1,24 @@
+# WRITE ITEM_IN_COMMON FUNCTION HERE #
+#                                    #
+#                                    #
+#                                    #
+#                                    #
+######################################
+
+
+
+
+list1 = [1,3,5]
+list2 = [2,4,5]
+
+
+print(item_in_common(list1, list2))
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    True
+
+"""
