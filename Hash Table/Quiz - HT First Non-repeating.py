ab55a0aa7b18b0b554ad71026419775890c73e31
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-04-29 18:54:17 +0800

    [Shubham][29-4] Hash Table - First Non repeating

--- Hash Table/Quiz - HT First Non-repeating.py
+++ Hash Table/Quiz - HT First Non-repeating.py
@@ -1,11 +1,15 @@
-# WRITE THE FUNCTION HERE #
-#                         #
-#                         #
-#                         #
-#                         #
-###########################
+def first_non_repeating_char(s):
+    dict={}
+    final=[]
+    for i in s:
+        dict[i]=dict.get(i, 0) + 1
 
-
+    result = "".join(str(key) for key, count in dict.items() if count == 1)
+    ans=[i for i in result]
+    if ans:
+        return ans[0]
+    else:
+        return None
 
 print( first_non_repeating_char('leetcode') )
 print( first_non_repeating_char('hello') )
