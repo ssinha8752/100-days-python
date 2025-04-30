e1098ed9adfa8ae442feafa44d421d38f0ca0a56
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-04-30 23:23:40 +0800

    [Shubham][30-4] Hash Table - Two Sum

---
+++ Hash Table/Quiz - HT Two Sum.py
@@ -0,0 +1,35 @@
+def two_sum(l,t):
+    dict={}
+    for j,i in enumerate(l):
+        comp=t-i
+        if comp in l and comp not in dict and i not in dict:
+            k=l.index(comp)
+            if k>j:
+                return [j,k]
+        dict[i]=True
+    return []
+
+print(two_sum([5, 1, 7, 2, 9, 3], 10))
+print(two_sum([4, 2, 11, 7, 6, 3], 9))
+print(two_sum([10, 15, 5, 2, 8, 1, 7], 12))
+print(two_sum([1, 3, 5, 7, 9], 10))
+print(two_sum([1, 2, 3, 4, 5], 10))
+print(two_sum([1, 2, 3, 4, 5], 7))
+print(two_sum([1, 2, 3, 4, 5], 3))
+print(two_sum([], 0))
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [1, 4]
+    [1, 3]
+    [0, 3]
+    [1, 3]
+    []
+    [2, 3]
+    [0, 1]
+    []
+
+"""
+
+
