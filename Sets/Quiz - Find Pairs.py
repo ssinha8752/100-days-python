abff0ca4bba61a09a27ab5a6455c5c7062059bfc
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-05-04 21:31:06 +0800

    [Shubham][4-5] Set

---
+++ Sets/Quiz - Find Pairs.py
@@ -0,0 +1,25 @@
+# WRITE FIND_PAIRS FUNCTION HERE #
+#                                #
+#                                #
+#                                #
+#                                #
+##################################
+
+
+
+
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [2, 4, 6, 8, 10]
+target = 7
+
+pairs = find_pairs(arr1, arr2, target)
+print (pairs)
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [(5, 2), (3, 4), (1, 6)]
+
+"""
