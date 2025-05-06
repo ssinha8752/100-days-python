5b15969fd4d4e6c480d0bfd59b970aaf5de3bdc6
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-05-06 21:13:59 +0800

    [Shubham][6-5] Graph

---
+++ Graph/Graph.py
@@ -0,0 +1,17 @@
+class Graph:
+    def __init__(self):
+        self.adj_list={}
+
+    def print(self):
+        for vertex in self.adj_list:
+            print(vertex,':',self.adj_list[vertex])
+
+    def add_vertex(self,v):
+        if v not in self.adj_list.keys():
+            self.adj_list[v]=[]
+            return True
+        return False
+
+my_graph=Graph()
+my_graph.add_vertex('A')
+my_graph.print()
