247735d598b0c5e9b40c4fa6a5dbd085d35381ff
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-05-08 23:00:30 +0800

    [Shubham][7-5] Graph

--- Graph/Practice.py
+++ Graph/Practice.py
@@ -1,15 +1,18 @@
-def fizzBuzz(n):
-    result=[]
-    for i in range(1,n+1):
-        if i%3==0 and i%5==0:
-            result.append("FizzBuzz")
-        elif i%5==0:
-            result.append("Buzz")
-        elif i%3==0:
-            result.append("Fizz")
-        else:
-            result.append(str(i))
-    return result
+def largestTriangleArea(points):
+    n = len(points)
+    max_area = 0
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            for k in range(j + 1, n):
+                # Shoelace formula to calculate triangle area
+                x1, y1 = points[i]
+                x2, y2 = points[j]
+                x3, y3 = points[k]
+                area = abs((x1 * y2 + x2 * y3 + x3 * y1) - (y1 * x2 + y2 * x3 + y3 * x1)) / 2
+                max_area = max(max_area, area)
+
+    return max_area
 
 
-print(fizzBuzz(3))
+print(largestTriangleArea([[0,0],[0,1],[1,0],[0,2],[2,0]]))
