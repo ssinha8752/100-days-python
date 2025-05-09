de9d963688218cadfe3e346a186f429dd861d4f0
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-05-09 22:00:05 +0800

    [Shubham][9-5] Graph

--- Graph/Practice.py
+++ Graph/Practice.py
@@ -1,18 +1,14 @@
-def largestTriangleArea(points):
-    n = len(points)
-    max_area = 0
-
-    for i in range(n):
-        for j in range(i + 1, n):
-            for k in range(j + 1, n):
-                # Shoelace formula to calculate triangle area
-                x1, y1 = points[i]
-                x2, y2 = points[j]
-                x3, y3 = points[k]
-                area = abs((x1 * y2 + x2 * y3 + x3 * y1) - (y1 * x2 + y2 * x3 + y3 * x1)) / 2
-                max_area = max(max_area, area)
-
-    return max_area
+import math
 
 
-print(largestTriangleArea([[0,0],[0,1],[1,0],[0,2],[2,0]]))
+def constructRectangle(area):
+    L=math.sqrt(area)
+    while L>0:
+        if area%L==0:
+            W=area//L
+            if L>=W:
+                return [int(L),int(W)]
+        L-=1
+    return []
+
+print(constructRectangle(4))
