80d192f92d65c79a06adaff0d662ecd5efeb18c2
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-05-09 20:45:15 +0800

    [Shubham][9-5] Graph

--- Graph/Quiz - Graph Remove Edge.py
+++ Graph/Quiz - Graph Remove Edge.py
@@ -23,12 +23,15 @@ class Graph:
             return True
         return False
 
-    ## WRITE REMOVE_EDGE METHOD HERE ##
-    #                                 #
-    #                                 #
-    #                                 #
-    #                                 #
-    ###################################
+    def remove_edge(self, v1,v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            try:
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+            except ValueError:
+                pass
+            return True
+        return False
 
 
 my_graph = Graph()
