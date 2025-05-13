bf08eaebd1ad2a0e48b727cca01eb8ffa4111a25
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-05-13 21:22:19 +0800

    [Shubham][13-5] Heap

--- Heaps/Practice.py
+++ Heaps/Practice.py
@@ -1,9 +1,11 @@
 def maximumProduct(nums):
-    prod=1
-    max3=sorted(nums, key=abs, reverse=True)[:-3]
-    for i in max3:
-        prod=prod*i
-    print(max3)
-    return prod
+    nums.sort()  # Sort the array in ascending order
 
-print(maximumProduct([-100,-98,-1,2,3,4]))
+    # Case 1: Product of the top three largest values
+    option1 = nums[-1] * nums[-2] * nums[-3]
+
+    # Case 2: Product of two smallest values (which may be negative) and the largest value
+    option2 = nums[0] * nums[1] * nums[-1]
+    return max(option1, option2)
+
+print(maximumProduct([-1,-2,-3]))
