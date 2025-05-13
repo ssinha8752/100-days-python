7dbd900b463011e45aebc2fc724c8bd7d237d5d9
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-05-13 21:46:03 +0800

    [Shubham][13-5] Heap

--- Heaps/Heap.py
+++ Heaps/Heap.py
@@ -1,55 +1,60 @@
 class MaxHeap:
     def __init__(self):
-        self.heap=[]
+        self.heap = []
 
-    def _left_child(self,index):
-        return 2*index+1
+    def _left_child(self, index):
+        return 2 * index + 1
 
     def _right_child(self, index):
         return 2 * index + 2
 
-    def _parent(self,index):
-        return (index-1)//2
+    def _parent(self, index):
+        return (index - 1) // 2
 
     def _swap(self, index1, index2):
-        self.heap[index1], self.heap[index2]=self.heap[index2],self.heap[index1]
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
 
     def _sink_down(self, index):
-        max_index=index
+        max_index = index
         while True:
-            left_index=self._left_child(index)
-            right_index=self._right_child(index)
+            left_index = self._left_child(index)
+            right_index = self._right_child(index)
 
-            if self.heap[left_index]>self.heap[max_index]:
-                max_index=left_index
+            # Ensure indices are within bounds before comparison
+            if left_index < len(self.heap) and self.heap[left_index] > self.heap[max_index]:
+                max_index = left_index
 
-            if (right_index<len(self.heap) and self.heap[right_index]>self.heap[max_index]):
-                max_index=right_index
+            if right_index < len(self.heap) and self.heap[right_index] > self.heap[max_index]:
+                max_index = right_index
 
-            if max_index!=index:
+            if max_index != index:
                 self._swap(index, max_index)
-                index=max_index
+                index = max_index
+            else:
+                break
 
-    def insert(self,value):
+    def insert(self, value):
         self.heap.append(value)
-        current=len(self.heap)-1
+        current = len(self.heap) - 1
 
-        while current>0 and self.heap[current] > self.heap[self._parent(current)]:
-            self._swap(current,self._parent(current))
-            current=self._parent(current)
+        while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
+            self._swap(current, self._parent(current))
+            current = self._parent(current)
 
     def remove(self):
-        if len(self.heap)==0:
+        if len(self.heap) == 0:
             return None
-        if len(self.heap)==1:
+        if len(self.heap) == 1:
             return self.heap.pop()
-        max_val=self.heap[0]
-        self.heap[0]=self.heap.pop()
-        self.__sink_down(0)
+
+        max_val = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
         return max_val
 
 
-myheap=MaxHeap()
+# Example usage
+myheap = MaxHeap()
 myheap.insert(99)
 myheap.insert(72)
 myheap.insert(61)
@@ -60,3 +65,7 @@ print(myheap.heap)
 myheap.insert(100)
 
 print(myheap.heap)
+
+myheap.remove()
+
+print(myheap.heap)  # Output after removal
