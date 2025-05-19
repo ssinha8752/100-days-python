2dbd1413ac321fe661cd24e49f2adf53451b2da4
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-05-19 23:02:52 +0800

    [Shubham][19-5] Heap - Min heap insert

--- Heaps/Heap - Min Heap remove.py
+++ Heaps/Heap - Min Heap remove.py
@@ -45,13 +45,18 @@ class MinHeap:
             else:
                 return
 
-    # WRITE THE REMOVE METHOD HERE #
-    #                              #
-    #                              #
-    #                              #
-    #                              #
-    ################################
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
 
+        if len(self.heap) == 1:
+            return self.heap.pop()
+
+        max_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+
+        return max_value
 
 
 myheap = MinHeap()
