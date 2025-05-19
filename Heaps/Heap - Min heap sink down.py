444cbc28178fda8ad06b2236e20f14b781a6b0e4
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-05-19 23:10:28 +0800

    [Shubham][19-5] Heap - Min heap insert

--- Heaps/Heap - Min heap sink down.py
+++ Heaps/Heap - Min heap sink down.py
@@ -22,12 +22,24 @@ class MinHeap:
             self._swap(current, self._parent(current))
             current = self._parent(current)
 
-    # WRITE THE _SINK_DOWN METHOD HERE #
-    #                                  #
-    #                                  #
-    #                                  #
-    #                                  #
-    ####################################
+    def _sink_down(self, index):
+        max_index = index
+        while True:
+            left_index = self._left_child(index)
+            right_index = self._right_child(index)
+
+            # Ensure indices are within bounds before comparison
+            if left_index > len(self.heap) and self.heap[left_index] < self.heap[max_index]:
+                max_index = left_index
+
+            if right_index > len(self.heap) and self.heap[right_index] < self.heap[max_index]:
+                max_index = right_index
+
+            if max_index != index:
+                self._swap(index, max_index)
+                index = max_index
+            else:
+                break
 
     def remove(self):
         if len(self.heap) == 0:
