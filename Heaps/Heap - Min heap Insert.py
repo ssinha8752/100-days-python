487d282ec2e39711950f357a215b72c7ec29d768
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-05-19 22:54:52 +0800

    [Shubham][19-5] Heap - Min heap insert

---
+++ Heaps/Heap - Min heap Insert.py
@@ -0,0 +1,48 @@
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def _left_child(self, index):
+        return 2 * index + 1
+
+    def _right_child(self, index):
+        return 2 * index + 2
+
+    def _parent(self, index):
+        return (index - 1) // 2
+
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
+
+    # WRITE THE INSERT METHOD HERE #
+    #                              #
+    #                              #
+    #                              #
+    #                              #
+    ################################
+
+
+myheap = MinHeap()
+myheap.insert(12)
+myheap.insert(10)
+myheap.insert(8)
+myheap.insert(6)
+
+print(myheap.heap)  # [6, 8, 10, 12]
+
+myheap.insert(4)
+
+print(myheap.heap)  # [4, 6, 10, 12, 8]
+
+myheap.insert(2)
+
+print(myheap.heap)  # [2, 6, 4, 12, 8, 10]
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [6, 8, 10, 12]
+    [4, 6, 10, 12, 8]
+    [2, 6, 4, 12, 8, 10]
+
+"""
