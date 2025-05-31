c772e1781c6fbdcde175cd6eb5637536bf942216
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-05-31 22:45:31 +0800

    [Shubham][31-05] Recursion

--- Recursion/rBST/delete.py
+++ Recursion/rBST/delete.py
@@ -1,12 +1,15 @@
 def __delete_node(self, current_node,value):
-    if current_node==None:
-        return None
     if value<current_node.value:
        current_node.left=self.__delete_node(current_node.left,value)
-    if value > current_node.value:
-        current_node.right = self.__delete_node(current_node.right, value)
     else:
-        return current_node
+        if current_node.left == None and current_node.right==None:
+            return None
+        elif current_node.left==None:
+            current_node=current_node.right
+        elif current_node.right==None:
+            current_node=current_node.left
+        else:
+            return current_node
 
 def delete_node(self,value):
     self.__delete_node()
