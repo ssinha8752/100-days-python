9ca09848f6ddea482304e47ebb85abc13b9dc9dc
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-06-06 16:47:33 +0800

    [Shubham][5-6] Recursion

--- Recursion/rBST/Quiz-Insert.py
+++ Recursion/rBST/Quiz-Insert.py
@@ -9,12 +9,19 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
 
-    ## WRITE R_INSERT METHODS HERE ##
-    #                               #
-    #                               #
-    #                               #
-    #                               #
-    #################################
+    def __r_insert(self, current_node, value):
+        if current_node == None:
+            return Node(value)
+        if value < current_node.value:
+            current_node.left = self.__r_insert(current_node.left, value)
+        if value > current_node.value:
+            current_node.right = self.__r_insert(current_node.right, value)
+        return current_node
+
+    def r_insert(self, value):
+        if self.root == None:
+            self.root = Node(value)
+        self.__r_insert(self.root, value)
 
 
 ##########################################################
