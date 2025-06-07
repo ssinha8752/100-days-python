57bd5232857df00b8588511e69ae6321b918d041
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-06-08 00:11:57 +0800

    [Shubham][8-6] Recursion

--- Recursion/rBST/Quiz - Delete.py
+++ Recursion/rBST/Quiz - Delete.py
@@ -23,17 +23,33 @@ class BinarySearchTree:
             self.root = Node(value)
         self.__r_insert(self.root, value)
 
-    def min_value(self, current_node):
+    def min_val(self, current_node):
         while (current_node.left is not None):
             current_node = current_node.left
         return current_node.value
 
-        ## WRITE DELETE_NODE METHODS HERE ##
-    #                                  #
-    #                                  #
-    #                                  #
-    #                                  #
-    ####################################
+    def __delete_node(self, current_node, value):
+        if current_node==None:
+            return None
+        if value < current_node.value:
+            current_node.left = self.__delete_node(current_node.left, value)
+        elif value > current_node.value:
+            current_node.right = self.__delete_node(current_node.right, value)
+        else:
+            if current_node.left == None and current_node.right == None:
+                return None
+            elif current_node.left == None:
+                current_node = current_node.right
+            elif current_node.right == None:
+                current_node = current_node.left
+            else:
+                sub_tree_main=self.min_val(current_node.right)
+                current_node.value=sub_tree_main
+                current_node.right=self.__delete_node(current_node.right, sub_tree_main)
+        return current_node
+
+    def delete_node(self, value):
+        self.__delete_node(self.root,value)
 
 
 ##########################################################
