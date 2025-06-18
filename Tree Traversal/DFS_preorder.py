ef77f041565a59d340124248a086b4e56673e8b8
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-06-18 22:17:17 +0800

    [Shubham][17-6] DFS

--- Tree Traversal/DFS_preorder.py
+++ Tree Traversal/DFS_preorder.py
@@ -63,7 +63,6 @@ class BinarySearchTree:
             if current_node.left is not None:
                 traverse(current_node.left)
             if current_node.right is not None:
-
                 traverse(current_node.right)
 
         traverse(self.root)
