e9e11b43c74603a36ed80ef681c0aa21e8c7f231
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-06-19 22:10:15 +0800

    [Shubham][19-6] BFS Postorder

--- Tree Traversal/DFS_postorder.py
+++ Tree Traversal/DFS_postorder.py
@@ -68,6 +68,19 @@ class BinarySearchTree:
         traverse(self.root)
         return  results
 
+    def DFS_post_order(self):
+        results = []
+
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+
+        traverse(self.root)
+        return  results
+
 my_tree=BinarySearchTree()
 my_tree.insert(47)
 my_tree.insert(21)
@@ -79,3 +92,4 @@ my_tree.insert(82)
 
 print(my_tree.BFS())
 print(my_tree.DFS_pre_order())
+print(my_tree.DFS_post_order())
