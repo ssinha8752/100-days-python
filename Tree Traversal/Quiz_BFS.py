7b063b7b26ac5e3ebca4c0bb002bd4d2a7c12723
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-06-21 17:34:44 +0800

    [Shubham][21-6] BFS Quiz

--- Tree Traversal/Quiz_BFS.py
+++ Tree Traversal/Quiz_BFS.py
@@ -42,7 +42,19 @@ class BinarySearchTree:
                 return True
         return False
 
-
+    def BFS(self):
+        current_node = self.root
+        queue=[]
+        results=[]
+        queue.append(current_node)
+        while len(queue)>0:
+            current_node=queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+        return results
 
 
 my_tree = BinarySearchTree()
