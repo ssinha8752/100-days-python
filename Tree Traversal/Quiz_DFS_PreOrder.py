56e756509403a74c6d4296913baf55f8bb66ad0b
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-06-22 21:42:26 +0800

    [Shubham][22-6] BFS Quiz

--- Tree Traversal/Quiz_DFS_PreOrder.py
+++ Tree Traversal/Quiz_DFS_PreOrder.py
@@ -57,12 +57,18 @@ class BinarySearchTree:
                 queue.append(current_node.right)
         return results
 
-    ### WRITE DFS_PRE_ORDER METHOD HERE ###
-    #                                     #
-    #                                     #
-    #                                     #
-    #                                     #
-    #######################################
+    def dfs_pre_order(self):
+        results = []
+
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+
+        traverse(self.root)
+        return results
 
 
 my_tree = BinarySearchTree()
