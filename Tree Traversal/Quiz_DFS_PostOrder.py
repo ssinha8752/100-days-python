ae1b05ab521b43d7e9965fed08720a0f09259e24
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-06-23 17:59:02 +0800

    [Shubham][22-6] BFS Quiz

--- Tree Traversal/Quiz_DFS_PostOrder.py
+++ Tree Traversal/Quiz_DFS_PostOrder.py
@@ -70,12 +70,18 @@ class BinarySearchTree:
         traverse(self.root)
         return results
 
-    ### WRITE DFS_POST_ORDER METHOD HERE ###
-    #                                      #
-    #                                      #
-    #                                      #
-    #                                      #
-    ########################################
+    def DFS_post_order(self):
+        results = []
+
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+
+        traverse(self.root)
+        return results
 
 
 my_tree = BinarySearchTree()
