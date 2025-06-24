b22fff3c268403703bf7ed61241aa923a2f5657f
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-06-24 16:47:32 +0800

    [Shubham][24-6] DFS Quiz

--- Tree Traversal/Quiz_DFS_InOrder.py
+++ Tree Traversal/Quiz_DFS_InOrder.py
@@ -83,12 +83,18 @@ class BinarySearchTree:
         traverse(self.root)
         return results
 
-    ### WRITE DFS_IN_ORDER METHOD HERE ###
-    #                                    #
-    #                                    #
-    #                                    #
-    #                                    #
-    ######################################
+    def DFS_in_order(self):
+        results = []
+
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            results.append(current_node.value)
+            if current_node.right is not None:
+                traverse(current_node.right)
+
+        traverse(self.root)
+        return results
 
 
 my_tree = BinarySearchTree()
