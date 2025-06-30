dacbb91fa71e239a5b5dd3fa6ddf4d8c8ba3a3d6
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-06-30 21:53:33 +0800

    [Shubham][30-6] Sorting

--- Sort/Inertion_sort.py
+++ Sort/Inertion_sort.py
@@ -1,4 +1,4 @@
-def inertion_sort(my_list):
+def insertion_sort(my_list):
     for i in range(1, len(my_list)):
         temp=my_list[i]
         j=i-1
@@ -6,5 +6,6 @@ def inertion_sort(my_list):
             my_list[j+1]=my_list[j]
             my_list[j]=temp
             j-=1
-        return my_list
+    return my_list
 
+print(insertion_sort([4,2,6,5,1,3]))
