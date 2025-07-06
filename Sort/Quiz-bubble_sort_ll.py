d021e6615fd9a9e2b01e311091b64b57534412f4
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-07-06 23:15:13 +0800

    [Shubham][6-7] Sorting Quiz

--- Sort/Quiz-bubble_sort_ll.py
+++ Sort/Quiz-bubble_sort_ll.py
@@ -27,12 +27,14 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
 
-    # WRITE BUBBLE_SORT METHOD HERE #
-    #                               #
-    #                               #
-    #                               #
-    #                               #
-    #################################
+    def bubble_sort(self):
+        for i in range(len(my_list) - 1, 0, -1):
+            for j in range(i):
+                if my_list[j] > my_list[j + 1]:
+                    temp = my_list[j]
+                    my_list[j] = my_list[j + 1]
+                    my_list[j + 1] = temp
+        return my_list
 
 
 my_linked_list = LinkedList(4)
