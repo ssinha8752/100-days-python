f8a63761011c10f40407658bbcf400ddaf58f1d4
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-07-10 15:41:29 +0800

    [Shubham][9-7] Sorting Quiz

---
+++ Sort/merge_sort.py
@@ -0,0 +1,21 @@
+def merge(l1,l2):
+    combined=[]
+    i=0
+    j=0
+    while i<len(l1) and j<len(l2):
+        if l1[i]<l2[j]:
+            combined.append(l1[i])
+            i+=1
+        else:
+            combined.append(l2[j])
+            j+= 1
+
+    while i<len(l1):
+        combined.append(l1[i])
+        i+=1
+
+    while j < len(l2):
+        combined.append(l2[j])
+        j+=1
+
+    return combined
