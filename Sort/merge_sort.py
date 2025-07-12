080051bd0998b6495222b5236b62ddf093d6eef4
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-07-12 17:47:16 +0530

    [Shubham][9-7] Sorting Quiz

--- Sort/merge_sort.py
+++ Sort/merge_sort.py
@@ -21,4 +21,11 @@ def merge(l1,l2):
     return combined
 
 
+def merge_sort(my_list):
+    mid_index=int(len(my_list)/2)
+    left=merge_sort(my_list[:mid_index])
+    right=merge_sort(my_list[mid_index:])
+
+    return merge(left,right)
+
 print(merge([1,3,5,7],[2,4,6,8]))
