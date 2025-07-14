fd1ddcff2b4adbd844eadbc505595bd7390932e4
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-07-14 23:52:37 +0530

    [Shubham][14-07] Sorting

--- Sort/quick_sort.py
+++ Sort/quick_sort.py
@@ -12,6 +12,12 @@ def pivot(my_list, p_i, e_i):
     swap(my_list, p_i, swap_index)
     return swap_index
 
+def quick_sort(my_list,l,r):
+    if l<r:
+        pivot_index = pivot(my_list, l, r)
+        quick_sort(my_list, l, pivot_index - 1)
+        quick_sort(my_list, pivot_index + 1, r)
+    return my_list
+
 my_list=[4,6,7,3,2,1,5]
-print(swap(my_list,0,6))
-print(my_list)
+print(quick_sort(my_list,0,len(my_list)-1))
