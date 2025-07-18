11e1a832c9c11797d6d586b9bc2c20ea0adc3403
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-07-19 02:22:47 +0530

    [Shubham][19-07] Fib

--- Dynamic Programming/Fibonacci.py
+++ Dynamic Programming/Fibonacci.py
@@ -1,7 +1,13 @@
+counter=0
+
 def fib(n):
+    global  counter
+    counter+=1
     if n==0 or n==1:
         return n
     return fib(n-1)+fib(n-2)
 
 # Example usage
+
 print(fib(10))  # Output: 55
+print(counter)
