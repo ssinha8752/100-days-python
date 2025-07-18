085f64e27b6f9b8f05ba2e9714dc00f4b2734a93
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-07-18 18:21:52 +0530

    [Shubham][18-07] FastAPI

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from models import User,Gender,Role
+from models import User,Gender,Role, UserUpdateRequest
 from typing import List
 from uuid import UUID, uuid4
 
@@ -39,3 +39,22 @@ async def delete_user(user_id:UUID):
         status_code=404,
         detail=f"{user_id} not present"
     )
+
+@app.put("/api/v1/users/{user_id}")
+async def update_user(user_update:UserUpdateRequest,user_id:UUID):
+    for user in db:
+        if user.id==user_id:
+            if user_update.first_name is not None:
+                user.first_name=user_update.first_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update.middle_name is not None:
+                user.middle_name = user_update.middle_name
+            if user_update.roles is not None:
+                user.roles = user_update.roles
+            return
+    raise HTTPException(
+        status_code=404,
+        detail=f"{user_id} not present"
+    )
+
