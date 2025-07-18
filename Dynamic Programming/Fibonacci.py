7cd31fecf1231d1a20eda8829084f5c17405c53d
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-07-19 02:29:21 +0530

    [Shubham][19-07] Fib

--- Dynamic Programming/Fibonacci.py
+++ Dynamic Programming/Fibonacci.py
@@ -1,11 +1,14 @@
 counter=0
-
+memo=[None]*100
 def fib(n):
     global  counter
     counter+=1
+    if memo[n] is not None:
+        return memo[n]
     if n==0 or n==1:
         return n
-    return fib(n-1)+fib(n-2)
+    memo[n]= fib(n-1)+fib(n-2)
+    return memo[n]
 
 # Example usage
 
