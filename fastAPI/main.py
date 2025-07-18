05e2d3f553d133d1c1b58ed89ac954bb05a17dc3
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-07-18 18:12:24 +0530

    [Shubham][18-07] FastAPI

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from models import User,Gender,Role
 from typing import List
 from uuid import UUID, uuid4
@@ -23,3 +23,19 @@ def root():
 @app.get("/api/v1/users")
 def fetch_user():
     return db
+
+@app.post("/api/v1/users")
+async def register_user(user: User):
+    db.append(user)
+    return {"id": user.id}
+
+@app.delete("/api/v1/users/{user_id}")
+async def delete_user(user_id:UUID):
+    for user in db:
+        if user.id==user_id:
+            db.remove(user)
+            return
+    raise HTTPException(
+        status_code=404,
+        detail=f"{user_id} not present"
+    )
