5bb3153ba9120b2e7350ea2be15fd6b2fa223ac2
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-07-23 19:13:11 +0530

    [Shubham][22-07] Remove last element

---
+++ Dynamic Programming/Max_min.py
@@ -0,0 +1,16 @@
+# WRITE FIND_MAX_MIN FUNCTION HERE #
+#                                  #
+#                                  #
+#                                  #
+#                                  #
+####################################
+
+
+print(find_max_min([5, 3, 8, 1, 6, 9]))
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    (9, 1)
+
+"""
