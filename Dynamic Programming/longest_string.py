41374f72f831cedaf8de1cb9ff23bc65f2828576
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-07-23 19:29:26 +0530

    [Shubham][23-07] Remove last element

--- Dynamic Programming/longest_string.py
+++ Dynamic Programming/longest_string.py
@@ -1,11 +1,5 @@
 def find_longest_string(s):
-    s_len=0
-    ans=""
-    for i in range(len(s)):
-        if len(s[i])>s_len:
-            s_len=len(s[i])
-            ans=s[i]
-    return ans
+    return max(s, key=len)
 
 string_list = ['apple', 'banana', 'kiwi', 'pear']
 longest = find_longest_string(string_list)
