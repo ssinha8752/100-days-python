d030d26831599e14dbb22a514155449bfb5a7941
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-07-25 23:22:24 +0530

    [Shubham][24-07] Max Profit

---
+++ Dynamic Programming/Max_Profit.py
@@ -0,0 +1,46 @@
+# WRITE MAX_PROFIT FUNCTION HERE #
+#                                #
+#                                #
+#                                #
+#                                #
+##################################
+
+
+prices = [7, 1, 5, 3, 6, 4]
+profit = max_profit(prices)
+print("Test with mixed prices:")
+print("Prices:", prices)
+print("Maximum profit:", profit)
+print("-----------------------------")
+
+prices = [7, 6, 4, 3, 1]
+profit = max_profit(prices)
+print("Test with descending prices:")
+print("Prices:", prices)
+print("Maximum profit:", profit)
+print("-----------------------------")
+
+prices = [1, 2, 3, 4, 5, 6]
+profit = max_profit(prices)
+print("Test with ascending prices:")
+print("Prices:", prices)
+print("Maximum profit:", profit)
+print("-----------------------------")
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Test with mixed prices:
+    Prices: [7, 1, 5, 3, 6, 4]
+    Maximum profit: 5
+    -----------------------------
+    Test with descending prices:
+    Prices: [7, 6, 4, 3, 1]
+    Maximum profit: 0
+    -----------------------------
+    Test with ascending prices:
+    Prices: [1, 2, 3, 4, 5, 6]
+    Maximum profit: 5
+    -----------------------------
+
+"""
