a119b6448c964555bed0c370336aba881400cae4
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-07-27 18:29:14 +0530

    [Shubham][27-07] Max Profit

---
+++ Dynamic Programming/Rotate.py
@@ -0,0 +1,19 @@
+# WRITE ROTATE FUNCTION HERE #
+#                            #
+#                            #
+#                            #
+#                            #
+##############################
+
+
+nums = [1, 2, 3, 4, 5, 6, 7]
+k = 3
+rotate(nums, k)
+print("Rotated array:", nums)
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Rotated array: [5, 6, 7, 1, 2, 3, 4]
+
+"""
