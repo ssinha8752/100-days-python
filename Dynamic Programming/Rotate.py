b6c4b81df94595926c85980bdc96ac1e87c29c7a
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-07-27 19:12:17 +0530

    [Shubham][27-07] Max Profit

--- Dynamic Programming/Rotate.py
+++ Dynamic Programming/Rotate.py
@@ -1,7 +1,10 @@
+def rotate(nums, k):
+    n = len(nums)
+    k = k % n
+    nums[:] = nums[-k:] + nums[:-k]
 
-
-nums = [1, 2, 3, 4, 5, 6, 7]
-k = 3
+nums = [1, 2, 3, 4, 5]
+k = 2
 rotate(nums, k)
 print("Rotated array:", nums)
 
