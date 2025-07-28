9fe43ae53b41ed0c75bb0c957aa1240def402dd2
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-07-28 23:29:03 +0530

    [Shubham][28-07] Max Sub

--- Dynamic Programming/Max_Sub.py
+++ Dynamic Programming/Max_Sub.py
@@ -1,4 +1,9 @@
-
+def max_subarray(nums):
+    max_current = max_global = nums[0]
+    for num in nums[1:]:
+        max_current = max(num, max_current + num)
+        max_global = max(max_global, max_current)
+    return max_global
 
 # Example 1: Simple case with positive and negative numbers
 input_case_1 = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
