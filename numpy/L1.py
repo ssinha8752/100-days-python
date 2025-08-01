ce418c66ae9ccc963568ef094d7179c1dd067f00
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-08-01 18:36:25 +0530

    [Shubham][1-08] numpy

--- numpy/L1.py
+++ numpy/L1.py
@@ -7,6 +7,11 @@ print(arr)
 
 a2=np.array([[4,5,6],[1,2,3]])
 print(a2)
+print(a2.shape)
+
+a2.shape=(3,2)
+print(a2)
+
 
 arr_zerp=np.zeros((4,5))
 print(arr_zerp)
@@ -16,3 +21,23 @@ print(np.arange(10,20,2))
 print(np.linspace(100,200,11))
 
 print(np.full((4,5),5))
+
+
+#Mathematical operations
+a5=np.array([1,2,3])
+a6=np.array([5,6,7])
+a7=np.array([1,2,3])
+
+print(np.equal(a5,a7))
+
+print(np.array_equal(a5,a7))
+
+print(np.sum(a5))
+
+a8=[[1,2],[8,9]]
+print(np.sum(a8))
+
+print(np.sum(a8,axis=0))
+print(np.sum(a8,axis=1))
+
+#sub,multiply,divide,sin,cos,tan,sqrt,exp,log,min,median,min,max, std, array1 + array2
