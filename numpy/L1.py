b55095952c4f223a9837e47d6bad9a5d83d8a00a
ssinha8752 <shubhamsinha8752@yahoo.com>
2025-08-01 17:53:51 +0530

    [Shubham][31-07] numpy

--- numpy/L1.py
+++ numpy/L1.py
@@ -1,1 +1,18 @@
 #numerical python - fundamental package for scientific computing in python
+
+import numpy as np
+
+arr=np.array([4,5,6])
+print(arr)
+
+a2=np.array([[4,5,6],[1,2,3]])
+print(a2)
+
+arr_zerp=np.zeros((4,5))
+print(arr_zerp)
+
+print(np.arange(10,20,2))
+
+print(np.linspace(100,200,11))
+
+print(np.full((4,5),5))
